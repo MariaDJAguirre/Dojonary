a7effa73e57b7a17e8a95507191a5b6744b0f4ae
Maria Aguirre <mercy27.ma@gmail.com>
2022-12-21 17:53:27 -0800

    Added likes function

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-var button= document.querySelector("button");
 
-console.log(button);
+const btn = document.getElementById('likeButton');
+let CountButtonLikesClicks=0;
 
-function signin(element) {
-    // element.style.backgroundColor = "blue"
-    element.classList.add("Signin");
-    console.log(signin);
+function likes(el) {
+    CountButtonLikesClicks += 1;
+    console.log(CountButtonLikesClicks);
+      btn.textContent = 'likes ' + CountButtonLikesClicks;
 
-}
-function Singout(element) {
-    element.classList.add("Signout");
-    console.log(Signout);
 
 }
 
-function AddDefinition (element) {
-    element.remove("btn");
-}
-function 13likes(element) {
-    element.classList.add("btn");
-    alert("Ninja was liked")
-    // function add1() {
-    // count++;
-    // countElement.innerText = "The count is" + count
-    console.log("13likes");
-}
-function 37likes(element) {
-    element.classList.add("btn");
-    console.log("37likes");
-
-}
